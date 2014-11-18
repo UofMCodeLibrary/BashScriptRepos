c20eda762d1258fa57475ba43209bfcddbb70a3a
slwstctt <slwstctt@memphis.edu>
2014-11-18 01:10:28 -0600

    Clarifications
    Altered code to be more readable. Added comments.

--- source/bubblesort.cpp
+++ source/bubblesort.cpp
@@ -1,20 +1,28 @@
+// A simple bubble sort demonstration.
+
 #include <stdio.h>
  
 int main()
 {
-  int array[100], n, c, d, swap;
+  // A few utility variables are created.
+  int n, swap;
  
   printf("Enter number of elements\n");
   scanf("%d", &n);
- 
+
+  // The number of elements is n. An array of n integers is created, and then we prompt for n integers.
+  int array[n]
+
   printf("Enter %d integers\n", n);
  
-  for (c = 0; c < n; c++)
+  for (int c = 0; c < n; c++) {
     scanf("%d", &array[c]);
- 
-  for (c = 0 ; c < ( n - 1 ); c++)
+  }
+
+  // Here the sort happens. Move up from the beginning.
+  for (int c = 0 ; c < ( n - 1 ); c++)
   {
-    for (d = 0 ; d < n - c - 1; d++)
+    for (int d = 0 ; d < n - c - 1; d++)
     {
       if (array[d] > array[d+1]) /* For decreasing order use < */
       {
@@ -25,10 +33,12 @@ int main()
     }
   }
  
+  // Give back the sorted list.
   printf("Sorted list in ascending order:\n");
  
-  for ( c = 0 ; c < n ; c++ )
+  for (int c = 0 ; c < n ; c++ ) {
      printf("%d\n", array[c]);
+  }
  
   return 0;
 }
